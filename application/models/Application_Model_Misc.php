6007bcc8e475cd366dbb7ce0a4a2c9cb75a712b7
cubes1 <cubes1@cubes1-Lenovo-E50-80>
2016-09-08 16:23:00 +0200

    cetvrtak 08.septembar 01

---
+++ application/models/Application_Model_Misc.php
@@ -0,0 +1,23 @@
+<?php
+
+	class Application_Model_Misc {
+		
+		public static function cleanURL($string) {
+			$url = str_replace ( "'", '', $string );
+			$url = str_replace ( '%20', ' ', $url );
+			$url = preg_replace ( '~[^\\pL_]+~u', '-', $url ); // substitutes anything but letters, numbers and '_' with separator
+			$url = trim ( $url, "-" );
+			$url = iconv ( "utf-8", "us-ascii//TRANSLIT", $url ); // you may opt for your own custom character map for encoding.
+			//$url = strtolower ( $url );
+			$url = preg_replace ( '~[^-a-zA-Z_]+~', '', $url ); // keep only letters, numbers, '_' and separator
+			$url = preg_replace('~\b[a-zA-Z]{1,2}\b\s*~', '', $url);
+			$url = str_replace ( '--', '-', $url );
+			$url = str_replace ( '--', '-', $url );
+
+			$url = $this->cleanLastDart($url);
+
+			return $url;
+		}
+		
+	}
+
